a67e490c1a26d2b86de044d82fe277e9aa9952e4
Heidi Anderson <pacificword70@gmail.com>
2022-06-30 02:56:52 -0700

    setting up site structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LifeComponent } from './pages/life/life.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { WorkComponent } from './pages/work/work.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'life', component: LifeComponent},
+  { path: 'work', component: WorkComponent},
+  { path: '**', component : PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { LifeComponent } from './pages/life/life.component';
+import { WorkComponent } from './pages/work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    LifeComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
